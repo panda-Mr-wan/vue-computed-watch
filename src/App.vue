62fbaa1bb7b83d3d06ccfb1bd9665d1557ef37c4
panda-Mr-wan <2359944502@qq.com>
2021-01-04 20:59:12 +0800

    vue样式练习

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,8 @@
 <template>
   <div>
-    <input type="text" v-bind:disabled="flag">
-    <button @click="changeEdit">更改可编辑状态</button>
-    <hr>
-    <button @click="popArray">弹出一个数组元素</button>
-    <p v-if="stus.length==0">暂无数据.....</p>
-    <p v-else>
-      <ul>
-        <li v-for="(value,index) in stus" v-bind:key="index">
-          {{value}}
-        </li>
-      </ul>
-    </p>
-    <hr>
-    <a v-bind:href="url">点击百度一下</a>
-    <hr>
-    <textarea cols="30" rows="10" v-model="str" v-on:input="tips"></textarea>
-    <span v-if="num<=40">已输入{{num}}字</span>
-    <span v-else v-bind:class="red">已超出{{num-40}}字</span>
-    <hr>
-    <textarea cols="30" rows="5" v-on:keyup.13="cons"></textarea>
-    <hr>
-    <a v-bind:href="url" @click.prevent="show">百度以下</a>
-    <hr>
-    <form action="check.html" v-on:submit.prevent="tijiao">
-      用户名：<input type="text" v-model="userName"><span>{{userNameTips}}</span><br>
-      密码：<input type="password" v-model="password"><span>{{passwordTips}}</span><br>
-      <input type="submit" name="submit" value="登录">
-    </form>
-    <hr>
-    <p>{{name | check}}</p>
-    <hr>
-    <ul>
-      <li v-for="(value,index) in kemu" v-bind:key="index">
-        {{value | select}}
-      </li>
-    </ul>
-    <hr>
-    <a v-bind:href="urls | qurl">百度</a>
-    <hr>
+    <h3>App组件</h3>
+    <p>{{ fullName }}</p>
+    <button @click="changeName">改变姓名</button>
   </div>
 </template>
 
@@ -47,75 +11,32 @@
     name:"App",
     data(){
       return {
-        flag:false,
-        stus:["tom","jerry","marry","lucy"],
-        url:"http://www.baidu.com",
-        str:"",
-        num:0,
-        red:"red",
-        userName:"",
-        password:"",
-        userNameTips:"",
-        passwordTips:"",
-        name:"tom",
-        kemu:["数学","英语","语文","体育"],
-        urls:"baidu"
+        firstName:"赵",
+        lastName:"四"
       }
     },
     methods:{
-      changeEdit(){
-        if(this.flag){
-          this.flag=false
-        }else{
-          this.flag=true
-        }
-      },
-      popArray(){
-        this.stus.pop()
-      },
-      tips(){
-        this.num=this.str.length;
-      },
-      cons(){
-        console.log("你按了回车键")
-      },
-      show(){
-        console.log("被阻止了")
-      },
-      tijiao(){
-        if(this.userName==""){
-          this.userNameTips="用户名不能为空";
-        }
-        if(this.password==""){
-          this.passwordTips="用户名不能为空";
-        }
+      changeName(){
+        this.fullName="张三"
       }
     },
-    filters:{
-      check(data){
-        if(data=="tom"){
-          return "jarry"
+    computed:{
+      fullName:{
+        get(){
+          console.log("computed get方法被调用了");
+          return this.firstName+this.lastName;
+        },
+        set(val){
+          console.log("computed set方法被调用了");
+          let name=val.split("");
+          this.firstName=name[0];
+          this.lastName=name[1];
         }
-      },
-      select(data){
-        if(data=="语文"){
-          return "选修"+data;
-        }
-        return "必修"+data;
-      },
-      qurl(data){
-        return "http://www."+data+".com";
       }
     }
   }
 </script>
 
 <style scoped>
-  a{
-    text-decoration: none;
-    color:#f00;
-  }
-  .red{
-    color:#f00;
-  }
+  
 </style>
---
+++ src/Class.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <h3>Class组件</h3>
+        <p v-bind:class="[flag?class1:class2]">测试哈哈哈哈哈</p>
+        <hr>
+        <p v-bind:class="[{one:flag},class2]">嘻嘻嘻嘻嘻</p>
+        <hr>
+        <button @click="flag=!flag">改变flag值</button>
+        <hr>
+        <p v-bind:style="{ color:activeColor, fontSize:fontSize+'px', backgroundColor:bg  }">嘿嘿嘿嘿嘿嘿</p>
+        <hr>
+        <p v-bind:style="objectStyle1">测试哈哈哈哈</p>
+        <hr>
+        <p v-bind:style="[objectStyle1,objectStyle2]">测试嘿嘿嘿嘿嘿嘿 </p>
+        <hr>
+        <my-component v-bind:class="class3"></my-component>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+    Vue.component("my-component",{
+        template:`
+            <div>
+                <h3>my-component组件</h3>
+                <p>测试哈哈哈</p>
+            </div>
+        `
+    })
+    export default {
+        name:"Class",
+        data(){
+            return {
+                flag:true,
+                class1:"one",
+                class2:"two",
+                class3:"three",
+                activeColor:"#f9f",
+                fontSize:30,
+                bg:"#09f",
+                objectStyle1:{
+                    color:"#ff0",
+                    backgroundColor:"#f00",
+                    fontSize:"25px"
+                },
+                objectStyle2:{
+                    padding:"5px",
+                    border:"1px solid #09f"
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .two{
+        background:#09f;
+        color:#fff;
+    }
+    .one{
+        border:1px solid #f00;
+    }
+    .three{
+        color:#f0f;
+    }
+</style>
